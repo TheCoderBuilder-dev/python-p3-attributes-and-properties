dc80ab6d2bebbfce53e2ac826d97353b9b4f8e1d
TheCoderBuilder-dev <brianinesh@gmail.com>
2025-06-04 00:58:26 +0300

    attributes

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,33 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
+approved_breeds = [
+    "Mastiff", "Chihuahua", "Corgi", "Shar Pei",
+    "Beagle", "French Bulldog", "Pug", "Pointer"
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Unknown", breed="Mastiff"):
+        self.name = name      # Use property setter
+        self.breed = breed    # Use property setter
+
+    # Name property
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # Breed property
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, value):
+        if value in approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,30 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Unknown", job="Admin"):
+        self.name = name  # Use property setter
+        self.job = job    # Use property setter
+
+    # Name property with title case conversion
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # Job property
+    def get_job(self):
+        return self._job
+
+    def set_job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
